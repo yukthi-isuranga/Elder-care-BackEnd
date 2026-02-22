aa5dde8b1148b1829de356cb1f06e57a22d23a9f
Yukthi Isuranga Bombuwala <65551837+yukthi-isuranga@users.noreply.github.com>
2026-02-22 13:04:08 +0530

    workig changes tsconfig and pacage.json

---
+++ src/models/item.ts
@@ -0,0 +1,12 @@
+export interface Item {
+  id: number;
+  name: string;
+}
+
+export let items: Item[] = [
+  { id: 1, name: 'Wheelchair' },
+  { id: 2, name: 'Walking Stick' },
+  { id: 3, name: 'Hospital Bed' },
+  { id: 4, name: 'Blood Pressure Monitor' },
+  { id: 5, name: 'First Aid Kit' },
+];
