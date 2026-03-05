de613a312ac68acb468a3d0a42f86ea353b8824c
Yukthi Isuranga Bombuwala <65551837+yukthi-isuranga@users.noreply.github.com>
2026-03-05 19:40:09 +0530

    adminGetCgApprovalController

--- src/controllers/adminController.ts
+++ src/controllers/adminController.ts
@@ -2,6 +2,14 @@ import { NextFunction, Request, Response } from 'express';
 import { User } from '../generated/prisma/client';
 import { prisma } from '../config/prisma';
 
+interface ApprovalParams {
+  approvalId: string;
+}
+
+interface AdminUserData extends Request {
+  user?: User;
+}
+
 export const adminController = async (
   req: Request,
   res: Response,
@@ -13,10 +21,6 @@ export const adminController = async (
     return res.status(404).json({ message: 'Admin Not works' });
   }
 };
-
-interface AdminUserData extends Request {
-  user?: User;
-}
 
 export const adminGetAllCtController = async (
   req: AdminUserData,
@@ -58,6 +62,54 @@ export const adminGetAllCgController = async (
     return res.status(200).json({
       message: `${userData?.name} -${userData?.email}  Admin Get all CareGivers....works....`,
       allCaraTakersData,
+    });
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
+};
+
+export const adminGetAllCgApprovalsController = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const allCaraTakersApprovalData = await prisma.caregiver.findMany({
+      where: { status: 'SUBMITTED' },
+    });
+
+    if (!allCaraTakersApprovalData) {
+      return res.status(400).json({ message: 'No data Found...' });
+    }
+
+    return res.status(200).json({
+      message: `Admin Get all CareGivers Approvals....`,
+      allCaraTakersApprovalData,
+    });
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
+};
+
+export const adminGetCgApprovalController = async (
+  req: AdminUserData,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const { approvalId } = req.params;
+
+    const allCaraTakersApprovalData = await prisma.caregiver.findUnique({
+      where: { id: approvalId.toString() },
+    });
+
+    if (!allCaraTakersApprovalData) {
+      return res.status(400).json({ message: 'No data Found...' });
+    }
+
+    return res.status(200).json({
+      message: `Admin Get all CareGivers Approvals....`,
+      allCaraTakersApprovalData,
     });
   } catch (error) {
     return res.status(400).json({ error });
