e33fe0402c8ea177ebc3d2118cfaee69556f331e
Yukthi Isuranga Bombuwala <65551837+yukthi-isuranga@users.noreply.github.com>
2026-03-05 14:09:28 +0530

    get-all-CareTaker-profiles-By-admin

--- src/controllers/adminController.ts
+++ src/controllers/adminController.ts
@@ -1,4 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
+import { User } from '../generated/prisma/client';
+import { prisma } from '../config/prisma';
 
 export const adminController = async (
   req: Request,
@@ -9,5 +11,32 @@ export const adminController = async (
     return res.status(201).json({ message: 'Admin Works' });
   } catch (error) {
     return res.status(404).json({ message: 'Admin Not works' });
+  }
+};
+
+interface AdminUserData extends Request {
+  user?: User;
+}
+
+export const adminGetAllCtController = async (
+  req: AdminUserData,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const userData = req.user;
+
+    const allCaraTakersData = await prisma.caretaker.findMany();
+
+    if (!allCaraTakersData) {
+      return res.status(400).json({ message: 'No data Found...' });
+    }
+
+    return res.status(200).json({
+      message: `${userData?.name} -${userData?.email}  Admin Get all Caretakers....works....`,
+      allCaraTakersData,
+    });
+  } catch (error) {
+    return res.status(400).json({ error });
   }
 };
--- src/routes/adminRoutes.ts
+++ src/routes/adminRoutes.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express';
-import { adminController } from '../controllers/adminController';
+import {
+  adminController,
+  adminGetAllCtController,
+} from '../controllers/adminController';
 import { authMiddleware } from '../middlewares/authMiddleware';
 
 const router = Router();
@@ -7,5 +10,12 @@ const router = Router();
 router.use(authMiddleware);
 
 router.get('/', adminController);
+
+// Todo [SCRUM-33]:  get all CareTaker profiles By admin
+router.get('/caretakers', adminGetAllCtController);
+
+// Todo [SCRUM-34] Get all Caregiver Profiles by Admin
+
+// Todo [SCRUM-35] Get All Approvals from Caregivers to Admin
 
 export default router;
