56859a0194fe3f87aea1f6729b974bd1e638e30b
Yukthi Isuranga Bombuwala <65551837+yukthi-isuranga@users.noreply.github.com>
2026-03-09 20:55:52 +0530

    generateToken changes

--- src/app.ts
+++ src/app.ts
@@ -14,8 +14,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
+    origin: process.env.FRONTEND_URL, // your Next.js frontend
+    credentials: true, // important for cookies
   }),
 );
 
